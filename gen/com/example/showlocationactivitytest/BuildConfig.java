1bdbf8907ec9ec5de024a52b9a9cf0bbad4467a0
rdshwetang <shwetang.bms@gmail.com>
2014-08-01 15:10:41 -0300

    uploading project show location activity

---
+++ gen/com/example/showlocationactivitytest/BuildConfig.java
@@ -0,0 +1,10 @@
+/* 
+ * @author Shwetang R.D 
+ * Visual Sistemas Electronicos Ltda.
+ * Belo Horizonte-Brazil -2014
+ */
+package com.example.showlocationactivitytest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
